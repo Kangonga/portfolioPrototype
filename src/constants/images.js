826da8f3d00ed1bfa4d1327cafadc68be4525005
Kangonga <guchu413@gmail.com>
2023-01-22 17:11:13 +0300

    use personal picture in header

--- src/constants/images.js
+++ src/constants/images.js
@@ -23,7 +23,6 @@ import about02 from '../assets/about02.png';
 import about03 from '../assets/about03.png';
 import about04 from '../assets/about04.png';
 
-import profile from '../assets/profile.png';
 import circle from '../assets/circle.svg';
 import logo from '../assets/logo.png';
 
@@ -35,7 +34,10 @@ import nb from '../assets/nb.png';
 import skype from '../assets/skype.png';
 import spotify from '../assets/spotify.png';
 
+import myPic from '../assets/myPic.jpg';
+
 export default {
+  myPic,
   email,
   mobile,
   api,
@@ -59,7 +61,6 @@ export default {
   about02,
   about03,
   about04,
-  profile,
   circle,
   logo,
   adidas,
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -48,7 +48,7 @@ const Header = () => {
           transition={{duration:0.5,delayChildren:0.5}}
         >
 
-          <img src={images.profile} alt="profile_bg" />
+          <img src={images.myPic} alt="profile_bg" />
           <motion.img
             whileInView={{scale:[0,1]}}
             transition={{duration:1,ease:'easeInOut'}}
